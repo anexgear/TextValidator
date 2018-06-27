edfebcdc9ee4a2d12e1da5de3e8cd4d3dd29c6ed
Igor Golovach <anexgear@gmail.com>
2018-06-27 11:43:20 +0300

    addPasswordCheck

--- TextValidator/src/main/java/mu/zz/axin/textvalidator/TextValidator.java
+++ TextValidator/src/main/java/mu/zz/axin/textvalidator/TextValidator.java
@@ -1,8 +1,24 @@
 package mu.zz.axin.textvalidator;
 
+
+
 public class TextValidator {
-    TextType textType;
-    PasswordStrength passwordStrength = PasswordStrength.WEAK;
+    private TextType textType;
+    private PasswordStrength passwordStrength = PasswordStrength.WEAK;
+    private String digitPolicy = "(?=.*[0-9])";
+    private String lowercaseLettersPolicy = "(?=.*[a-z])";
+    private String uppercaseLettersPolicy = "(?=.*[A-Z])";
+    private String specialCharactersPolicy = "(?=.*[!@#$%^&*_+=])";
+    private String noWhitespacePolicy = "(?=\\S+$)";
+    private String longPasswordLengthPolicy = ".{8,25}";
+    private String normalPasswordLengthPolicy = ".{5,25}";
+    private String shortPasswordLengthPolicy = ".{2,25}";
+    private String start = "(";
+    private String end = ")";
+    private String startOr = "[";
+    private String endOr = "]";
+
+
 
     public enum TextType {
         PASSWORD,
@@ -32,16 +48,75 @@ public class TextValidator {
 //        .{6,20}             # anything, at least eight places though
     }
 
+    private String passwordPolicyBuilder(PasswordStrength passwordStrength){
+        StringBuilder passwordPolicy = new StringBuilder(start);
+        switch (passwordStrength) {
+            case WEAK:
+                passwordPolicy.append(startOr)
+                        .append(digitPolicy)
+                        .append(lowercaseLettersPolicy)
+                        .append(uppercaseLettersPolicy)
+                        .append(specialCharactersPolicy)
+                        .append(endOr)
+                        .append(noWhitespacePolicy)
+                        .append(shortPasswordLengthPolicy)
+                        .append(end);
+                return passwordPolicy.toString();
+            case NORMAL:
+                passwordPolicy.append(start)
+                        .append(startOr)
+                        .append(digitPolicy)
+                        .append(lowercaseLettersPolicy)
+                        .append(uppercaseLettersPolicy)
+                        .append(specialCharactersPolicy)
+                        .append(endOr)
+                        .append(end)
+                        .append(start)
+                        .append(startOr)
+                        .append(digitPolicy)
+                        .append(lowercaseLettersPolicy)
+                        .append(uppercaseLettersPolicy)
+                        .append(specialCharactersPolicy)
+                        .append(endOr)
+                        .append(end)
+                        .append(noWhitespacePolicy)
+                        .append(normalPasswordLengthPolicy)
+                        .append(end);
+                return passwordPolicy.toString();
+            case STRONG:
+                passwordPolicy.append(digitPolicy)
+                        .append(lowercaseLettersPolicy)
+                        .append(uppercaseLettersPolicy)
+                        .append(specialCharactersPolicy)
+                        .append(noWhitespacePolicy)
+                        .append(longPasswordLengthPolicy)
+                        .append(end);
+                return passwordPolicy.toString();
+            default:
+                return null;
+        }
+    }
+
     public boolean isTextValid(String text) {
+        String pattern;
         switch (textType) {
             case PASSWORD:
                 switch (passwordStrength){
                     case WEAK:
-                        return false;
+                        pattern = passwordPolicyBuilder(PasswordStrength.WEAK);
+                        System.out.println(pattern);
+                        assert pattern != null;
+                        return text.matches(pattern);
                     case NORMAL:
-                        return false;
+                        pattern = passwordPolicyBuilder(PasswordStrength.NORMAL);
+                        System.out.println(pattern);
+                        assert pattern != null;
+                        return text.matches(pattern);
                     case STRONG:
-                        return false;
+                        pattern = passwordPolicyBuilder(PasswordStrength.STRONG);
+                        System.out.println(pattern);
+                        assert pattern != null;
+                        return text.matches(pattern);
                     default:
                         return false;
                 }
--- example/src/main/java/mu/zz/axin/example/MainActivity.java
+++ example/src/main/java/mu/zz/axin/example/MainActivity.java
@@ -2,17 +2,26 @@ package mu.zz.axin.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import mu.zz.axin.textvalidator.TextValidator;
 
 public class MainActivity extends AppCompatActivity {
+    TextView weakPswd, normalPswd, strongPswd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        weakPswd = findViewById(R.id.weakPassword);
+        normalPswd = findViewById(R.id.normalPassword);
+        strongPswd = findViewById(R.id.strongPassword);
+
         TextValidator validator = new TextValidator().setTextType(TextValidator.TextType.PASSWORD)
-                .setPasswordStrength(TextValidator.PasswordStrength.STRONG);
-        validator.isTextValid("E");
+                .setPasswordStrength(TextValidator.PasswordStrength.WEAK);
+        weakPswd.setText(String.valueOf(validator.isTextValid("123")));
+        normalPswd.setText(String.valueOf(validator.isTextValid("1234asdf")));
+        strongPswd.setText(String.valueOf(validator.isTextValid("123!@#qweQWE")));
+
     }
 }
